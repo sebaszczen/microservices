00443f3232961c9449d34f1a60a64155d54a207e
sebaszczen <sebastianszczebiot@gmail.com>
2019-06-25 16:02:01 +0200

    czesciowo dziala zipkin

--- currency-exchange-service/src/test/java/sebaszczen/currencyexchangeservice/CurrencyExchangeServiceApplicationTests.java
+++ currency-exchange-service/src/test/java/sebaszczen/currencyexchangeservice/CurrencyExchangeServiceApplicationTests.java
@@ -1,13 +1,8 @@
 package sebaszczen.currencyexchangeservice;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CurrencyExchangeServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
